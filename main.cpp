7248765758855151c74a76217f68923d766ac497
GtJohnny <148626933+GtJohnny@users.noreply.github.com>
2024-03-20 04:30:16 +0200

    Update main.cpp
    A sad and abrupt commit

--- main.cpp
+++ main.cpp
@@ -1,57 +1,360 @@
 #include <iostream>
-#include <array>
+#include <string>
+#include <vector>
+#include <stack>
 
-#include <Helper.h>
+
+
+enum tip{cloak,architect,assassin,old}charm_type;
+
+class Item{
+    std::string Name;
+    std::string Description;
+    int usages;
+public:
+    friend void UseItem();
+};
+
+class Charm{
+public:
+    Charm()=default;
+    Charm(const std::string &denumire, const std::string &descriere,const tip &type=old, int impact=1) : Denumire(denumire), Descriere(descriere), type(type),   impact(impact) {}
+    Charm(const Charm&obj){
+        this->impact=obj.impact;
+        this->Denumire=obj.Denumire;
+        this->type=obj.type;
+        this->Descriere=obj.Descriere;
+    }
+
+    Charm& operator=(const Charm& obj) {
+        if(this!=&obj) {
+            this->Denumire = obj.Denumire;
+            this->Descriere = obj.Descriere;
+            this->type = obj.type;
+            this->impact = obj.impact;
+        }
+        return *this;
+    }
+
+    const Charm operator+(const Charm& obj)
+    {
+        Charm c;
+        c.Descriere=obj.Descriere;
+        c.type=obj.type;
+        c.Denumire=obj.Denumire;
+        c.impact=this->impact+obj.impact;
+        return c;
+    }
+
+    const Charm operator+(const int&nr){
+        Charm c;
+        c.Descriere=this->getDescriere();
+        c.type=this->type;
+        c.Denumire=this->Denumire+"+";
+        c.impact=this->impact+nr;
+        return c;
+    };
+
+    const std::string &getDenumire() const {
+        return Denumire;
+    }
+
+    void setDenumire(const std::string &denumire) {
+        Denumire = denumire;
+    }
+
+    const std::string &getDescriere() const {
+        return Descriere;
+    }
+
+    void setDescriere(const std::string &descriere) {
+        Descriere = descriere;
+    }
+
+    tip getType() const {
+        return type;
+    }
+
+    void setType(tip type) {
+        Charm::type = type;
+    }
+
+    int getImpact() const {
+        return impact;
+    }
+
+    void setImpact(int impact) {
+        Charm::impact = impact;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const Charm &charm) {
+        out << "Charm: " << charm.Denumire << "\nDescriere: " << charm.Descriere << "\nDe tip: " << charm.type << "\nCu: " << charm.impact<<'\n';
+        return out;
+    }
+
+private:
+    std::string Denumire;
+    std::string Descriere;
+    tip type=old;
+    int impact=0;
+
+
+
+};
+
+class Crest{
+    std::string description;
+    tip crest_type;
+    std::stack<Charm> inv;
+public:
+    Crest(const tip&crest):crest_type(crest){}
+
+    const std::string &getDescription() const {
+        return description;
+    }
+
+    void setDescription(const std::string &description) {
+        Crest::description = description;
+    }
+
+    tip getCrestType() const {
+        return crest_type;
+    }
+
+    void setCrestType(tip crestType) {
+        crest_type = crestType;
+    }
+
+    const std::stack<Charm> &getInv() const {
+        return inv;
+    }
+
+    void setInv(const std::stack<Charm> &inv) {
+        Crest::inv = inv;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const Crest &crest) {
+        out << "Descriere: " << crest.description << "\nTip: " << crest.crest_type << "Lista charms:\n";
+        for(int i=0;i<crest.inv.size();i++) {
+            out << crest.inv.top()<<'\n';
+        }
+        if(crest.inv.empty()){
+            out<<"Gol\n";
+        }
+        return out;
+    };
+
+
+};
+
+class Enemy{
+    int hp,armor,agility,evade;
+public:
+    friend void AttackPlayer();
+    friend void Death();
+};
+
+class Point{
+    int x,y;
+public:
+    Point()=default;
+    Point(const int&x,const int&y):x(x),y(y){}
+
+};
+
+class Location{
+    Point p;
+    int room;
+public:
+    Location()=default;
+    Location(const Point&p,int &i):p(p),room(i){}
+
+};
+
+
+class Bloodstain{
+    int money;
+    Location loc;
+    bool collected;
+public:
+    Bloodstain(const int& money,const Location&loc):money(money),collected(false),loc(loc){};
+    friend void Collect();
+
+    ~Bloodstain(){
+        if(!collected){
+            if(this->money>200){
+                std::cout<<"\nA great deal of shells have been lost\n";
+            }
+            if(this->money>100){
+                std::cout<<"\nAn honest sum of shells has been lost\n";
+            }if(this->money>50){
+                std::cout<<"\nA speck of dust and money may have been forgotten\n";
+            }
+        }
+    }
+
+};
+
+
+class Player{
+    std::string nume;
+    int health;
+    int max_health;
+    int speed;
+    int money;
+    int damage;
+    float silk; //mana
+    float max_silk;
+    int armor;
+    tip equippedCrest;
+    Bloodstain *ptr;
+    friend Bloodstain;
+    Location loc;
+
+public:
+    //ToDo
+    void Jump();
+    void Walk();
+
+    int getHealth() const {
+        return this->health;
+    }
+
+    int getMaxHealth() const {
+        return this->max_health;
+    }
+
+    int getSpeed() const {
+        return this->speed;
+    }
+
+    int getMoney() const {
+        return this->money;
+    }
+
+    float getSilk() const {
+        return this->silk;
+    }
+
+    float getMaxSilk() const {
+        return this->max_silk;
+    }
+
+    void setHealth(int health) {
+        this->health = health;
+    }
+
+    void setMaxHealth(int maxHealth) {
+        this->max_health = maxHealth;
+    }
+
+    void setSpeed(int speed) {
+        this->speed = speed;
+    }
+
+    void setMoney(int money) {
+        this->money = money;
+    }
+
+    void setSilk(float silk) {
+        this->silk = silk;
+    }
+
+    void setMaxSilk(float maxSilk) {
+        this->max_silk = maxSilk;
+    }
+
+    const std::string &getNume() const {
+        return this->nume;
+    }
+
+    void setNume(const std::string &nume) {
+        this->nume = nume;
+    }
+
+    tip getEquippedCharm() const {
+        return this->equippedCrest;
+    }
+
+    void setEquippedCharm(tip equippedCrest) {
+        this->equippedCrest = equippedCrest;
+    }
+ //   Player player("Hornet",5,5,0,15,old,12);
+
+    Player(const std::string &nume,const int &hp,const int & max_hp, const float &silk, const float &max_silk,const tip&eq ,const int &damage) {
+        this->nume = nume;
+        this->health = hp;
+        this->silk = silk;
+        this->max_silk = max_silk;
+        this->max_health = max_hp;
+        this->equippedCrest = eq;
+        this->damage = damage;
+        this->money = 0;
+        this->speed = 10;
+        this->armor = 0;
+    }
+
+
+    void Spawn ();
+    void Death(){
+        if(this->ptr !=NULL)
+            delete this->ptr;
+        this->ptr=new Bloodstain(this->money,this->loc);
+    }
+    void Retrieval(){
+
+    }
+    friend void EquipCrest();
+    friend void PlayerAttack();
+
+    int getDamage() const {
+        return damage;
+    }
+
+    void setDamage(int damage) {
+        Player::damage = damage;
+    }
+
+    void EquipCrest(const Crest&crest){
+        this->equippedCrest=crest.getCrestType();
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const Player &player) {
+        out << "Viata: " << player.health << "/" << player.max_health<< "\nsilk: " << player.silk << "/"<< player.max_silk << "\nBani: " << player.money << "\nDamage: " << player.damage <<"\n\n";
+        return out;
+    }
+
+};
+
+
+
 
 int main() {
-    std::cout << "Hello, world!\n";
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
+    /// select items, customizable, select crest
+
+
+    Player player("Hornet",5,5,0,15,old,12);
+    std::cout<<player;
+    Crest crest1(assassin);
+    player.EquipCrest(crest1);
+    Charm c1("Den1","Des",cloak,7);
+    std::cout<<c1;
+    Charm c2("Apex","an",assassin,10);
+    std::cout<<c2;
+    Charm c4("D3","Des",old,8);
+    Charm c3=c2+c1;
+    std::cout<<c3;
+    /*
+    player.attack()
+
+    */
+    if(player.getHealth()==0) {
+        player.Death();
+        player.Retrieval();
     }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
-    }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
-    ///                Exemplu de utilizare cod generat                     ///
-    ///////////////////////////////////////////////////////////////////////////
-    Helper helper;
-    helper.help();
-    ///////////////////////////////////////////////////////////////////////////
-    return 0;
+
+
+
+
+
 }
