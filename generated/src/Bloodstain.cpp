d60e679068292a93f064b42c11746dd4eb73c7ee
GtJohnny <148626933+GtJohnny@users.noreply.github.com>
2024-04-17 08:35:11 +0300

    Update Bloodstain.cpp

--- generated/src/Bloodstain.cpp
+++ generated/src/Bloodstain.cpp
@@ -8,14 +8,15 @@
 ///@param map where the player died
 ///@param money how much it was lost
 ///@param point where precisely it happened
-Bloodstain::Bloodstain(const Point& point,const Map& map,int money=0):locatie(point),map(map),money(money){
-    //**
+Bloodstain::Bloodstain(const Point& point,const Map& map,int money=0):locatie(point),map(map){
+    Bloodstain::money=money;
 };
             ///@param player Player that collects lost money
 [[nodiscard]] int Bloodstain::Collect(Player &player) {
 
-    if (this->money > 200) {
+    if (Bloodstain::money > 200) {
         std::cout << "\nA great deal of shells have been lost\n";
+        Bloodstain::money=0;
     }
     if (this->money > 100) {
         std::cout << "\nAn honest sum of shells has been lost\n";
@@ -23,11 +24,9 @@ Bloodstain::Bloodstain(const Point& point,const Map& map,int money=0):locatie(po
     if (this->money > 50) {
         std::cout << "\nA speck of dust and money may have been forgotten\n";
     }
-    return player.money += this->money;
-}
-
-int Bloodstain::getMoney() const {
-    return money;
+    delete player.reper;
+    return player.money += Bloodstain::money;
+    
 }
 
 const Point &Bloodstain::getLocatie() const {
