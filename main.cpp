2941ed2d1f4150661591761fe289c28c4da12858
GtJohnny <ionutxd04@gmail.com>
2024-05-29 04:48:31 +0300

    Update M3
    update with textures

--- main.cpp
+++ main.cpp
@@ -3,11 +3,53 @@
 #include <vector>
 #include <stack>
 //#include "Player.h"
-#include "Enemy.h"
+//#include "Enemy.h"
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+
+void loadTexture(sf::RenderWindow& window, const std::string& fileName,int x,int y, int w, int l){
+    sf::Texture tx;
+    tx.loadFromFile(fileName);
+    sf::IntRect (x,y,w,l);
+    sf::Sprite sp(tx);
+    auto size = window.getSize();
+    sp.setScale(size.x/tx.getSize().x,size.y/tx.getSize().y);
+    window.draw(sp);
+}
+
 
 
 int main() {
 
+    sf::RenderWindow window(sf::VideoMode(1024, 768), "My window");
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            switch(event.type){
+                case sf::Event::Closed:
+                    window.close();
+                break;
+                case sf::Event::MouseButtonPressed:
+
+                break;
+
+                default:
+            }
+
+            sf::IntRect (0,0,1024,768);
+            loadTexture(window,"poze/meniu.png",0,0,1024,768);
+            loadTexture(window, "QuitGame.png", 800, 300, 200, 70);
+            loadTexture(window,"StartGame.png",800,400,200,70);
+
+
+
+            window.close();
+        }
+    }
 
 }
 
